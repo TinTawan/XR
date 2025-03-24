3b05ca0f11feecfa5d6f7273cc17d45caf86e216
ameera <as4770c@gre.ac.uk>
2025-03-24 06:53:14 +0000

    added wheel and menu
    temporary wheel and menu - menu disappears when wheel is turned

---
+++ XR_Pir/Assets/Scripts/MenuTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class MenuTrigger : MonoBehaviour
+{
+    public GameObject canvas;
+    public Transform wheelTransform;
+    public float rotationThreshold = 90f;
+    private bool canvasHidden = false;
+    private float startRotation;
+
+    void Start()
+    {
+        startRotation = wheelTransform.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        float wheelRotation = wheelTransform.eulerAngles.y - startRotation;
+
+        if (wheelRotation > 180)
+        {
+            wheelRotation -= 360;
+        }
+
+        if (!canvasHidden)
+        {
+            if (Mathf.Abs(wheelRotation) > rotationThreshold)
+            {
+                canvas.SetActive(false);
+                canvasHidden = true;
+            }
+        }
+    }
+}
