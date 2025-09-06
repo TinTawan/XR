2b9dccc3a9eec8ddce706469649c00e9fd1131e2
Justin Ward <jw4566c@gre.ac.uk>
2025-09-06 18:47:15 +0100

    uses a list to contain the enabled deck positions
    instead of enabling and disabling the game obejcts, it now populates a list with the correct positions

--- XR_Pir/Assets/Scripts/NPC_MoveToPlayer.cs
+++ XR_Pir/Assets/Scripts/NPC_MoveToPlayer.cs
@@ -6,21 +6,23 @@ public class NPC_MoveToPlayer : MonoBehaviour
 {
     [SerializeField] Transform player, npc;
 
-    [SerializeField] List<Transform> npcTransforms;
+    [SerializeField] List<Transform> upperDeckTransforms = new List<Transform>(), lowerDeckTransforms = new List<Transform>();
     [SerializeField] GameObject upperDeckPositions, lowerDeckPositions;
     Transform startTransform;
 
 
+    List<Transform> enabledDeckTransforms;
 
     private void Start()
     {
-        startTransform = npcTransforms[0];
+        startTransform = upperDeckTransforms[0];
         npc.transform.position = startTransform.position;
+
     }
 
     private void Update()
     {
-        if(player.position.y > 8)
+        if (player.position.y > 8)
         {
             EnableDeck(0);
         }
@@ -37,17 +39,44 @@ public class NPC_MoveToPlayer : MonoBehaviour
         switch (deckIndex)
         {
             case 0:
-                upperDeckPositions.SetActive(true);
-                lowerDeckPositions.SetActive(false);
+                enabledDeckTransforms = new List<Transform>();
+
+                for(int i = 0; i < upperDeckTransforms.Count; i++)
+                {
+                    enabledDeckTransforms.Add(upperDeckTransforms[i]);
+                }
                 break;
             case 1:
-                upperDeckPositions.SetActive(false);
-                lowerDeckPositions.SetActive(true);
+                enabledDeckTransforms = new List<Transform>();
+
+                for (int i = 0; i < lowerDeckTransforms.Count; i++)
+                {
+                    enabledDeckTransforms.Add(lowerDeckTransforms[i]);
+                }
                 break;
             default:
-                upperDeckPositions.SetActive(true);
-                lowerDeckPositions.SetActive(false);
+                enabledDeckTransforms = new List<Transform>();
+
+                for (int i = 0; i < upperDeckTransforms.Count; i++)
+                {
+                    enabledDeckTransforms.Add(upperDeckTransforms[i]);
+                }
                 break;
         }
+    }
+
+    Transform ShortestDistance(List<Transform> enabledTransforms)
+    {
+        Transform closestItem = null;
+        float shortest = Mathf.Infinity;
+
+        foreach(Transform item in enabledTransforms)
+        {
+            //Vector3.Distance(player.position, item.position);
+            shortest = Mathf.Min(shortest, Vector3.Distance(player.position, item.position));
+            closestItem = item;
+        }
+
+        return closestItem;
     }
 }
