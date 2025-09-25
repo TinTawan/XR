26c52a617f61382351ce19b68b9b564bdeb49a30
Justin Ward <jw4566c@gre.ac.uk>
2025-09-25 14:27:55 +0100

    amended respawn point
    now the respawn point is on the upper deck in case the player loses the telescope before opening the lower deck

--- XR_Pir/Assets/Scripts/FallenOffShipRespawn.cs
+++ XR_Pir/Assets/Scripts/FallenOffShipRespawn.cs
@@ -44,9 +44,13 @@ public class FallenOffShipRespawn : MonoBehaviour
         yield return new WaitForSeconds(respawnTime);
         StartCoroutine(AppertureZoomOut(0.1f));
 
+        if(go.TryGetComponent(out Rigidbody rb))
+        {
+            rb.velocity = Vector3.zero;
+        }
+
         yield return new WaitForEndOfFrame();
         go.transform.position = respawnPoint.position;
-
 
     }
 
